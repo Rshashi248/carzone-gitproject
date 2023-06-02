7e61d8ff343c0d1a9aae94bdc33d432da9f31878
Rshashi248 <rshashi248@gmail.com>
2023-06-03 02:45:07 +0530

    completed Teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -121,6 +121,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
+# media setting
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
